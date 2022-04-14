b7d3b0e01b9792064fc6871f0021d19a10c3f343
shafiq.imtiaz <shafiqimtiaz@gmail.com>
2022-04-14 08:06:44 -0400

    add more factorials

--- Java/Dalhousie Interview/Factorial.java
+++ Java/Dalhousie Interview/Factorial.java
@@ -8,9 +8,14 @@ public class Factorial {
         System.out.println("Please enter a number");
         int n = input.nextInt();
 
-        System.out.print("Iterative Factorial of " + n + " is " + iterativefactorial(n));
+        System.out.print("Iterative For Factorial of " + n + " is " + iterativeForfactorial(n));
+        System.out.println();
+        System.out.print("Iterative While Factorial of " + n + " is " + iterativeWhilefactorial(n));
+        System.out.println();
+        System.out.print("One Line Factorial of " + n + " is " + oneLinefactorial(n));
         System.out.println();
         System.out.print("Recursive Factorial of " + n + " is " + recursivefactorial(n));
+        
 
         input.close();
     }
@@ -21,11 +26,26 @@ public class Factorial {
         return (n * recursivefactorial(n - 1));
     }
 
-    static int iterativefactorial(int n) {
+    static int iterativeForfactorial(int n) {
         int fact = 1;
         for (int i = 2; i <= n; i++)
             fact *= i;
         return fact;
+    }
+
+    static int iterativeWhilefactorial(int n) {
+        if (n == 0)
+            return 1;
+        int i = n, fact = 1;
+        while (n / i != n) {
+            fact = fact * i;
+            i--;
+        }
+        return fact;
+    }
+
+    static int oneLinefactorial(int n) {
+        return (n == 1 || n == 0) ? 1 : n * oneLinefactorial(n - 1);
     }
 
 }
