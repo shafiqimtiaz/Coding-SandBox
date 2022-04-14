0613263abb8e71218130034121c18a62e4f581e0
shafiq.imtiaz <shafiqimtiaz@gmail.com>
2022-04-14 08:22:16 -0400

    refactor charFreq

--- Java/Dalhousie Interview/CharFreqArr.java
+++ Java/Dalhousie Interview/CharFreqArr.java
@@ -10,11 +10,14 @@ public class CharFreqArr {
         String[] words = str.split(" ");
 
         for (String w : words) {
+            
             // Convert the given string into character array
             char[] ch = w.toCharArray();
             int[] freq = new int[w.length()];
 
             for (int i = 0; i < w.length(); i++) {
+
+                // Counts the characters and their corresponding frequency
                 freq[i] = 1;
                 for (int j = i + 1; j < w.length(); j++) {
                     if (ch[i] == ch[j]) {
@@ -23,9 +26,8 @@ public class CharFreqArr {
                         ch[j] = '0';
                     }
                 }
-            }
-            // Displays the characters and their corresponding frequency
-            for (int i = 0; i < freq.length; i++) {
+
+                // Displays the characters and their corresponding frequency
                 if (ch[i] != ' ' && ch[i] != '0') {
                     System.out.print(ch[i] + "(" + freq[i] + ") ");
                 }
