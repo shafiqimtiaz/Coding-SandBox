2a49f4ffb9239745b872d8067da6f1d02a62cf2e
shafiq.imtiaz <shafiqimtiaz@gmail.com>
2022-04-27 00:52:58 -0400

    add responsive animated hamburger navbar

--- PHP/CGA/includes/footer.php
+++ PHP/CGA/includes/footer.php
@@ -5,6 +5,28 @@
 </body>
 
 <script>
+    window.addEventListener("resize", resizeListener);
+
+    function resizeListener() {
+        let link = document.getElementById("header-link");
+        if (window.innerWidth > 1024) {
+            link.style.display = "flex";
+        } else {
+            link.style.display = "none";
+        }
+    }
+
+    function toggleHamburger(x) {
+        x.classList.toggle("change");
+        let link = document.getElementById("header-link");
+        if (link.style.display === "block") {
+            link.style.display = "none";
+        } else {
+            link.style.display = "block";
+            link.style.textAlign = "center";
+        }
+    }
+
     setTimeout(() => {
         const box = document.getElementById('notification');
         box.style.display = 'none';
--- PHP/CGA/includes/header.php
+++ PHP/CGA/includes/header.php
@@ -37,8 +37,15 @@ $role_id = $_SESSION['role_id'];
         <div class="title-bar">
             <h1><?= $role_name ?></h1>
             <nav>
-                <p>Welcome <b><?= $name ?></b></p>
-                <ul>
+                <div class="header-minimum">
+                    <p>Welcome <b><?= $name ?></b></p>
+                    <a href="javascript:void(0);" class="hamburger-icon" onclick="toggleHamburger(this)">
+                        <div class="hamburger bar1"></div>
+                        <div class="hamburger bar2"></div>
+                        <div class="hamburger bar3"></div>
+                    </a>
+                </div>
+                <ul class="header-link" id="header-link">
                     <li><a href="javascript:history.go(-1)">&larr;</a></li>
                     <li><a href="?page=home">Home</a></li>
                     <li><a href="../auth/reset-email.php">Reset Email</a></li>
