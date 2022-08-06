9813de19afbae353fca50598f793ce64ca1f15d3
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2022-08-06 01:01:39 -0400

    added LC and refactor folders

--- Java/LeetCode/containsDuplicate.java
+++ Java/LeetCode/containsDuplicate.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class containsDuplicate {
+public class containsDuplicate {
+
+    public static void containsDuplicate(int[] nums) {
+        for (int i : nums) {
+            System.out.print(i + " ");
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5,6};
+        containsDuplicate(arr);
+    }
 }
--- Java/Misc/Contact Manager/README.md
+++
@@ -1,2 +0,0 @@
-# contact-manager-starter
-Starter Project for the Junit 5 Tutorial
