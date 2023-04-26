9fbfbdec2abb681655fabc48cbe8db99cc132b38
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-04-26 15:33:27 -0400

    add generics driver

--- Java/Collections/src/example/Generics.java
+++ Java/Collections/src/example/Generics.java
@@ -1,5 +1,8 @@
 package example;
 
+import java.util.ArrayList;
+
+
 public class Generics {
     public static void main(String[] args) {
         // Create Integer arrays m1, m2
@@ -16,10 +19,50 @@ public class Generics {
         System.out.println("\nm1 * m2 is ");
         GenericMatrix.printResult(
                 m1, m2, integerMatrix.multiplyMatrix(m1, m2), '*');
+
+        ArrayList<Object> intArr = new ArrayList<>();
+        intArr.add(1);
+        intArr.add(2);
+        intArr.add(3);
+        intArr.add("String");
+        printResult(intArr);
+    }
+
+    public static void printResult(ArrayList<?> arr) {
+        System.out.println(arr);
     }
 }
 
 abstract class GenericMatrix<E extends Number> {
+    /**
+     * Print matrices, the operator, and their operation result
+     */
+    public static void printResult(
+            Number[][] m1, Number[][] m2, Number[][] m3, char op) {
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1[0].length; j++)
+                System.out.print(" " + m1[i][j]);
+
+            if (i == m1.length / 2)
+                System.out.print("  " + op + "  ");
+            else
+                System.out.print("     ");
+
+            for (int j = 0; j < m2.length; j++)
+                System.out.print(" " + m2[i][j]);
+
+            if (i == m1.length / 2)
+                System.out.print("  =  ");
+            else
+                System.out.print("     ");
+
+            for (int j = 0; j < m3.length; j++)
+                System.out.print(m3[i][j] + " ");
+
+            System.out.println();
+        }
+    }
+
     /**
      * Abstract method for adding two elements of the matrices
      */
@@ -34,7 +77,6 @@ abstract class GenericMatrix<E extends Number> {
      * Abstract method for defining zero for the matrix element
      */
     protected abstract E zero();
-
 
     /**
      * Add two matrices
@@ -86,35 +128,6 @@ abstract class GenericMatrix<E extends Number> {
         }
 
         return result;
-    }
-
-    /**
-     * Print matrices, the operator, and their operation result
-     */
-    public static void printResult(
-            Number[][] m1, Number[][] m2, Number[][] m3, char op) {
-        for (int i = 0; i < m1.length; i++) {
-            for (int j = 0; j < m1[0].length; j++)
-                System.out.print(" " + m1[i][j]);
-
-            if (i == m1.length / 2)
-                System.out.print("  " + op + "  ");
-            else
-                System.out.print("     ");
-
-            for (int j = 0; j < m2.length; j++)
-                System.out.print(" " + m2[i][j]);
-
-            if (i == m1.length / 2)
-                System.out.print("  =  ");
-            else
-                System.out.print("     ");
-
-            for (int j = 0; j < m3.length; j++)
-                System.out.print(m3[i][j] + " ");
-
-            System.out.println();
-        }
     }
 }
 
